cbaf092b451f7fe1c20658ae81ba1b2fe129df0e
LCsiu <148622803+LouisCaiKM7@users.noreply.github.com>
2025-10-31 08:41:48 +0800

    github deploy -2

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/motor-simulator' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/controller-simulation' : '',
   images: {
     unoptimized: true,
   },
